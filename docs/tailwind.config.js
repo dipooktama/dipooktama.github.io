ff3af257d988fd99480a9a3bb394efe32edde569
dipooktama <dipooktama@usu.ac.id>
2025-01-05 21:23:50 +0700

    finishing templating

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./page/**/*.html"],
+  content: ['./index.html',
+    './template/template.html',
+    './resume/*.html',
+    './page/**/*.html'
+  ],
   theme: {
     extend: {},
   },
